7174f033384e4ee0139b2a61418c8733bc338aad
rmpardee <ruthmpardee@gmail.com>
2016-05-11 15:09:54 -0700

    findNRooks passing tests.

--- src/solvers.js
+++ src/solvers.js
@@ -14,9 +14,29 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // an empty array we'll add the n row arrays to and return
+  var solution = [];
+  // create an array of numbers zero through n-1 (all the index options in a given row)
+  var indices = [];
+  for (var i = 0; i < n; i++) {
+    indices.push(i);
+  }
+  // shuffle that index array
+  indices = _.shuffle(indices);
+  // loop n times
+  for (var i = 0; i < n; i++) {
+    // each loop start with an empty row array
+    var rowArray = [];
+    // each loop, pop the last one off from the shuffled index array
+    var indexToPlaceRook = indices.pop();
+    // loop n times, pushing a 0 at each position, except a 1 at the popped off index
+    for (var j = 0; j < n; j++) {
+      (j === indexToPlaceRook) ? rowArray.push(1) : rowArray.push(0);
+    };
+    // push the row array we've built into the solution array
+    solution.push(rowArray);
+  }
+  // return the solution array
   return solution;
 };
 
